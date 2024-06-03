191048f071944d0b114d3653479336d292214dde
Thomas de Jaeger <dejaeger.thomas@gmail.com>
2024-06-03 12:33:20 +0200

    Add files via upload

---
+++ AUP_2024/data/labs/lab01/tests/q0.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q0',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> len(partners) > 0 \nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
